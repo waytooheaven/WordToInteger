3085b6fdb691b295d1af5c613aa0823cc09b6450
aprotr <anilcodingninja@yahoo.com>
2023-05-12 13:44:30 +0300

    .

---
+++ UnitTest/MainTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = WordToInteger.Program.ParseEnglish("I wrote two millions sentences");
+            Assert.AreEqual(result, "I wrote 2000000 sentences");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = WordToInteger.Program.ParseEnglish("I wrote two millions sentences");
+            Assert.AreNotEqual(result, "I wrote 2000001 sentences");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var result = WordToInteger.Program.ParseEnglish("I wrote two millions sentences for three dollars");
+            Assert.AreEqual(result, "I wrote 2000000 sentences for 3 dollars");
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var result = WordToInteger.Program.ParseEnglish("I bought forty cars");
+            Assert.AreEqual(result, "I bought 40 cars");
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var result = WordToInteger.Program.ParseEnglish("I bought fifty cars");
+            Assert.AreEqual(result, "I bought 50 cars");
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var result = WordToInteger.Program.ParseEnglish("I have three quadrillions homes");
+            Assert.AreEqual(result, "I have 3000000000000000 homes");
+        }
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ab759b0-f9da-4d86-a880-0f58f2f2e5b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
